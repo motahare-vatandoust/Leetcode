34b25fda16e105875f777f5498c9f8b911e545e2
moti.vatandoust <moti.vatandoust@gmail.com>
2025-03-01 14:48:23 +0330

    Implement function to increment an array representation of an integer by one

---
+++ array/plus-one.js
@@ -0,0 +1,19 @@
+// Input: digits = [1,2,3]
+// Output: [1,2,4]
+// Explanation: The array represents the integer 123.
+// Incrementing by one gives 123 + 1 = 124.
+// Thus, the result should be [1,2,4].
+
+var plusOne = function (digits) {
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] < 9) {
+      digits[i]++;
+      return digits;
+    }
+    digits[i] = 0;
+  }
+  digits.unshift(1); // if all digits are 9, then we need to add 1 to the front of the array
+  return digits;
+};
+
+console.log(plusOne([1, 2, 3])); // [1,2,4]
